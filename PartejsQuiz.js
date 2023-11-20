fb3718b48968fe4aea02ba8f0af3de2dc9b085c1
Yupi <126489588+YupiHere@users.noreply.github.com>
2023-11-20 01:59:50 -0300

    PartejsQuiz.js

---
+++ PartejsQuiz.js
@@ -0,0 +1,219 @@
+// Questão 1: Quanto é 30+10?
+
+document.getElementById("meuBotaoA1").addEventListener("click", function () {
+    var mensagemDiv = document.getElementById("mensagem");
+    mensagemDiv.textContent = "Errou :(";
+});
+
+document.getElementById("meuBotaoB1").addEventListener("click", function () {
+    var mensagemDiv = document.getElementById("mensagem2");
+    mensagemDiv.textContent = "Acertou!!!";
+});
+
+document.getElementById("meuBotaoC1").addEventListener("click", function () {
+    var mensagemDiv = document.getElementById("mensagem3");
+    mensagemDiv.textContent = "Errou :(";
+});
+
+document.getElementById("meuBotaoD1").addEventListener("click", function () {
+    var mensagemDiv = document.getElementById("mensagem4");
+    mensagemDiv.textContent = "Errou :(";
+});
+
+// Questão 2: Qual a capital do Brasil?
+
+document.getElementById("meuBotaoA2").addEventListener("click", function () {
+    var mensagemDiv = document.getElementById("mensagem5");
+    mensagemDiv.textContent = "Errou :(";
+});
+
+document.getElementById("meuBotaoB2").addEventListener("click", function () {
+    var mensagemDiv = document.getElementById("mensagem6");
+    mensagemDiv.textContent = "Errou :(";
+});
+
+document.getElementById("meuBotaoC2").addEventListener("click", function () {
+    var mensagemDiv = document.getElementById("mensagem7");
+    mensagemDiv.textContent = "Acertou!!!";
+});
+
+document.getElementById("meuBotaoD2").addEventListener("click", function () {
+    var mensagemDiv = document.getElementById("mensagem8");
+    mensagemDiv.textContent = "Errou :(";
+});
+
+// Questão 3: Qual é o maior planeta do nosso sistema solar?
+
+document.getElementById("meuBotaoA3").addEventListener("click", function () {
+    var mensagemDiv = document.getElementById("mensagem9");
+    mensagemDiv.textContent = "Acertou!!!";
+});
+
+document.getElementById("meuBotaoB3").addEventListener("click", function () {
+    var mensagemDiv = document.getElementById("mensagem10");
+    mensagemDiv.textContent = "Errou :(";
+});
+
+document.getElementById("meuBotaoC3").addEventListener("click", function () {
+    var mensagemDiv = document.getElementById("mensagem11");
+    mensagemDiv.textContent = "Errou :(";
+});
+
+document.getElementById("meuBotaoD3").addEventListener("click", function () {
+    var mensagemDiv = document.getElementById("mensagem12");
+    mensagemDiv.textContent = "Errou :(";
+});
+
+// Questão 4: Qual é o maior mamífero terrestre?
+
+document.getElementById("meuBotaoA4").addEventListener("click", function () {
+    var mensagemDiv = document.getElementById("mensagem13");
+    mensagemDiv.textContent = "Errou :(";
+});
+
+document.getElementById("meuBotaoB4").addEventListener("click", function () {
+    var mensagemDiv = document.getElementById("mensagem14");
+    mensagemDiv.textContent = "Errou :(";
+});
+
+document.getElementById("meuBotaoC4").addEventListener("click", function () {
+    var mensagemDiv = document.getElementById("mensagem15");
+    mensagemDiv.textContent = "Errou :(";
+});
+
+document.getElementById("meuBotaoD4").addEventListener("click", function () {
+    var mensagemDiv = document.getElementById("mensagem16");
+    mensagemDiv.textContent = "Acertou!!!";
+});
+
+// Questão 5: Qual é a cor resultante da mistura de azul e amarelo?
+
+document.getElementById("meuBotaoA5").addEventListener("click", function () {
+    var mensagemDiv = document.getElementById("mensagem17");
+    mensagemDiv.textContent = "Errou :(";
+});
+
+document.getElementById("meuBotaoB5").addEventListener("click", function () {
+    var mensagemDiv = document.getElementById("mensagem18");
+    mensagemDiv.textContent = "Acertou!!!";
+});
+
+document.getElementById("meuBotaoC5").addEventListener("click", function () {
+    var mensagemDiv = document.getElementById("mensagem19");
+    mensagemDiv.textContent = "Errou :(";
+});
+
+document.getElementById("meuBotaoD5").addEventListener("click", function () {
+    var mensagemDiv = document.getElementById("mensagem20");
+    mensagemDiv.textContent = "Errou :(";
+});
+
+// Questão 6: Qual é o continente mais populoso?
+
+document.getElementById("meuBotaoA6").addEventListener("click", function () {
+    var mensagemDiv = document.getElementById("mensagem21");
+    mensagemDiv.textContent = "Acertou!!!";
+});
+
+document.getElementById("meuBotaoB6").addEventListener("click", function () {
+    var mensagemDiv = document.getElementById("mensagem22");
+    mensagemDiv.textContent = "Errou :(";
+});
+
+document.getElementById("meuBotaoC6").addEventListener("click", function () {
+    var mensagemDiv = document.getElementById("mensagem23");
+    mensagemDiv.textContent = "Errou :(";
+});
+
+document.getElementById("meuBotaoD6").addEventListener("click", function () {
+    var mensagemDiv = document.getElementById("mensagem24");
+    mensagemDiv.textContent = "Errou :(";
+});
+
+// Questão 7: Qual é a fruta conhecida por ser amarela por fora e branca por dentro?
+
+document.getElementById("meuBotaoA7").addEventListener("click", function () {
+    var mensagemDiv = document.getElementById("mensagem25");
+    mensagemDiv.textContent = "Errou :(";
+});
+
+document.getElementById("meuBotaoB7").addEventListener("click", function () {
+    var mensagemDiv = document.getElementById("mensagem26");
+    mensagemDiv.textContent = "Errou :(";
+});
+
+document.getElementById("meuBotaoC7").addEventListener("click", function () {
+    var mensagemDiv = document.getElementById("mensagem27");
+    mensagemDiv.textContent = "Errou :(";
+});
+
+document.getElementById("meuBotaoD7").addEventListener("click", function () {
+    var mensagemDiv = document.getElementById("mensagem28");
+    mensagemDiv.textContent = "Acertou!!!";
+});
+
+// Questão 8: Qual estação do ano vem depois do inverno?
+
+document.getElementById("meuBotaoA8").addEventListener("click", function () {
+    var mensagemDiv = document.getElementById("mensagem29");
+    mensagemDiv.textContent = "Errou :(";
+});
+
+document.getElementById("meuBotaoB8").addEventListener("click", function () {
+    var mensagemDiv = document.getElementById("mensagem30");
+    mensagemDiv.textContent = "Errou :(";
+});
+
+document.getElementById("meuBotaoC8").addEventListener("click", function () {
+    var mensagemDiv = document.getElementById("mensagem31");
+    mensagemDiv.textContent = "Acertou!!!";
+});
+
+document.getElementById("meuBotaoD8").addEventListener("click", function () {
+    var mensagemDiv = document.getElementById("mensagem32");
+    mensagemDiv.textContent = "Errou :(";
+});
+
+// Questão 9: Qual esporte é jogado com uma bola e uma cesta?
+
+document.getElementById("meuBotaoA9").addEventListener("click", function () {
+    var mensagemDiv = document.getElementById("mensagem33");
+    mensagemDiv.textContent = "Acertou!!!";
+});
+
+document.getElementById("meuBotaoB9").addEventListener("click", function () {
+    var mensagemDiv = document.getElementById("mensagem34");
+    mensagemDiv.textContent = "Errou :(";
+});
+
+document.getElementById("meuBotaoC9").addEventListener("click", function () {
+    var mensagemDiv = document.getElementById("mensagem35");
+    mensagemDiv.textContent = "Errou :(";
+});
+
+document.getElementById("meuBotaoD9").addEventListener("click", function () {
+    var mensagemDiv = document.getElementById("mensagem36");
+    mensagemDiv.textContent = "Errou :(";
+});
+
+// Questão 10: Qual meio de transporte é conhecido por flutuar na água?
+
+document.getElementById("meuBotaoA10").addEventListener("click", function () {
+    var mensagemDiv = document.getElementById("mensagem37");
+    mensagemDiv.textContent = "Errou :(";
+});
+
+document.getElementById("meuBotaoB10").addEventListener("click", function () {
+    var mensagemDiv = document.getElementById("mensagem38");
+    mensagemDiv.textContent = "Errou :(";
+});
+
+document.getElementById("meuBotaoC10").addEventListener("click", function () {
+    var mensagemDiv = document.getElementById("mensagem39");
+    mensagemDiv.textContent = "Errou :(";
+});
+
+document.getElementById("meuBotaoD10").addEventListener("click", function () {
+    var mensagemDiv = document.getElementById("mensagem40");
+    mensagemDiv.textContent = "Acertou!!!";
+});
